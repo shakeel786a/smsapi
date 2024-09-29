aa1c4eabc041bdcae0cb4bb7aeab1b7fc675dc4c
ShakeelApi <ansari.shakeel0786@gmail.com>
2024-09-29 15:41:55 +0530

    code change with vercel.

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const connectDB = require("./db/connect");
-const productRoutes = require("./routes/products");
-const menuRoutes = require("./routes/menu");
-const userRoles = require("./routes/userRole");
-const users = require("./routes/users");
-const states = require("./routes/states");
-const city = require("./routes/city");
+const connectDB = require("./api/db/connect");
+const productRoutes = require("./api/routes/products");
+const menuRoutes = require("./api/routes/menu");
+const userRoles = require("./api/routes/userRole");
+const users = require("./api/routes/users");
+const states = require("./api/routes/states");
+const city = require("./api/routes/city");
 
 const PORT = process.env.PORT || 4000;
 
 // Middleware or to set router
 app.use(express.json());
 app.use(express.urlencoded());
-app.use("/api/products", productRoutes);
-app.use("/api/menu", menuRoutes);
-app.use("/api/userRoles", userRoles);
-app.use("/api/users", users);
-app.use("/api/states", states);
-app.use("/api/city", city);
+app.use("/api/v1/products", productRoutes);
+app.use("/api/v1/menu", menuRoutes);
+app.use("/api/v1/userRoles", userRoles);
+app.use("/api/v1/users", users);
+app.use("/api/v1/states", states);
+app.use("/api/v1/city", city);
 
 app.get("/", (req, res) => {
   res.send("Hi, I am live");
